5c4cfdbefa5d2b12d922529a3f713ffc6580cf59
sendkite <sendkite1@gmail.com>
2023-08-18 17:20:11 +0900

    feat add category with jsx

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
 import './style.css'
 
-const CATEGORIES = [
-    {name: "technology", color: "#3b82f6"},
-    {name: "science", color: "#16a34a"},
-    {name: "finance", color: "#ef4444"},
-    {name: "society", color: "#eab308"},
-    {name: "entertainment", color: "#db2777"},
-    {name: "health", color: "#14b8a6"},
-    {name: "history", color: "#f97316"},
-    {name: "news", color: "#8b5cf6"},
-];
-
 const initialFacts = [
     {
         id: 1,
@@ -73,9 +62,35 @@ function App() {
     );
 }
 
+const CATEGORIES = [
+    {name: "technology", color: "#3b82f6"},
+    {name: "science", color: "#16a34a"},
+    {name: "finance", color: "#ef4444"},
+    {name: "society", color: "#eab308"},
+    {name: "entertainment", color: "#db2777"},
+    {name: "health", color: "#14b8a6"},
+    {name: "history", color: "#f97316"},
+    {name: "news", color: "#8b5cf6"},
+];
+
 function CategoryFilter() {
     return (
-        <aside>Category filter</aside>
+        <aside>
+            <ul>
+                <li className="category">
+                    <button className="btn btn-all-categories">All</button>
+                </li>
+                {CATEGORIES.map((category) => (
+                    <li className="category" key={category.name}>
+                        <button
+                            className="btn btn-category"
+                            style={{backgroundColor: category.color}}>
+                            {category.name}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </aside>
     );
 }
 
@@ -96,7 +111,7 @@ function FactList() {
 }
 
 
-function Fact({ fact }) {
+function Fact({fact}) {
     return <li className="fact">
         <p>
             {fact.text}
