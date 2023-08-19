489dc3c96a4e5ef91a5dbbf6f90af704a72746a5
sendkite <sendkite1@gmail.com>
2023-08-19 22:50:12 +0900

    feat added loading components

--- src/App.js
+++ src/App.js
@@ -39,13 +39,20 @@ const initialFacts = [
 function App() {
     const [showForm, setShowForm] = useState(false);
     const [facts, setFacts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(function () {
         async function getFacts() {
+            setIsLoading(true);
             const { data: facts, error } = await supabase
                 .from('facts')
-                .select('*');
-            setFacts(facts)
+                .select('*')
+                .order('votesInteresting', {ascending: false})
+                .limit(1000);
+
+            if (!error) setFacts(facts);
+            else alert('There was a problem getting data');
+            setIsLoading(false);
         }
         getFacts();
     }, []);
@@ -53,14 +60,22 @@ function App() {
     return (
         <>
             <Header showForm={showForm} setShowForm={setShowForm}/>
-            {showForm ? <NewFactForm setFacts={setFacts} setShowForm={setShowForm} /> : null}
+            {showForm ?
+                <NewFactForm
+                    setFacts={setFacts}
+                    setShowForm={setShowForm}
+                /> : null}
 
             <main className={"main"}>
                 <CategoryFilter />
-                <FactList facts={facts} />
+                {isLoading ? <Loader /> : <FactList facts={facts} />}
             </main>
         </>
     );
+}
+
+function Loader() {
+    return <p className="message">Loading...</p>;
 }
 
 function Header({showForm, setShowForm}) {
